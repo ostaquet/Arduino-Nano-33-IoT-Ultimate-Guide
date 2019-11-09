cf7c275ae69db605034c8d4df8adcdc9a202f774
Olivier Staquet <ostaquet@gmail.com>
2019-11-09 16:17:04 +0100

    Add image

--- src/IMU_ShakeDetector/IMU_ShakeDetector.ino
+++ src/IMU_ShakeDetector/IMU_ShakeDetector.ino
@@ -17,20 +17,21 @@
 #include <Arduino_LSM6DS3.h>
 
 void setup() {
-  // Initialize Serial for log
-  Serial.begin(115600);
-  while (!Serial);
-
-  // Initialize IMU
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
-
   // Initialize embedded LED
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   delay(50);
+  
+  // Initialize IMU
+  if (!IMU.begin()) {
+    // Failed to initialize IMU, blink the internal LED
+    while (1) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
+  }
 }
 
 void loop() {
